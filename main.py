e732d6f7dabbce08664ce5663fa1aa88ec1d820b
Lucas Maia <lucas.mdantas@icloud.com>
2023-12-15 10:07:15 -0300

    construção da função pegar_cotacao

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
+import requests
 
-lista_moedas = ['USD', 'EUR']
+requisicao = requests.get('https://economia.awesomeapi.com.br/json/all')
+dict_moedas = requisicao.json()
+
+lista_moedas = list(dict_moedas.keys())
+
 
 def pegar_cotacao():
-    pass
+    moeda = combobox_selec_moeda.get()
+    data_cotacao = calendario_moeda.get()
+    ano = data_cotacao[-4:]
+    mes = data_cotacao[3:5]
+    dia = data_cotacao[:2]
+    link = f'https://economia.awesomeapi.com.br/json/daily/{moeda}-BRL/?start_date={ano}{mes}{dia}&end_date={ano}{mes}{dia}'
+    requisicao_moeda = requests.get(link)
+    cotacao = requisicao_moeda.json()
+    valor_moeda = float(cotacao[0]['bid'])
+    label_cotacao_output['text'] = f"Cotação de {moeda} no dia {data_cotacao}: R$ {valor_moeda:,.2f}"
 
 def selecionar_arquivo():
     pass
@@ -18,8 +32,8 @@ janela.title('Cotação de Moedas')
 
 # cotação de uma moeda
 
-title1 = tk.Label(text='Cotação de 1 Moeda Específica', borderwidth=2, relief='solid')
-title1.grid(row=0, column=0, padx=10, pady=10, sticky='nswe', columnspan=3)
+title1 = tk.Label(text='Cotação de 1 Moeda Específica', borderwidth=2, relief='solid', fg='white', bg='black')
+title1.grid(row=0, column=0, pady=10, sticky='nswe', columnspan=3)
 
 label_selec_moeda = tk.Label(text='Selecione a Moeda Desejada: ', anchor='e')
 label_selec_moeda.grid(row=1, column=0, padx=10, pady=10, sticky='nsew', columnspan=2)
@@ -41,8 +55,8 @@ btn_pegar_cotacao.grid(row=3, column=2, padx=10, pady=10, sticky='nsew')
 
 # cotação de várias moedas
 
-title2 = tk.Label(text='Cotação de Múltiplas Moedas', borderwidth=2, relief='solid')
-title2.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=3)
+title2 = tk.Label(text='Cotação de Múltiplas Moedas', borderwidth=2, relief='solid', fg='white', bg='black')
+title2.grid(row=4, column=0, pady=10, sticky='nswe', columnspan=3)
 
 label_selec_arquivo = tk.Label(text='Selecione um arquivo Excel com as Moedas na Coluna A...', anchor='e')
 label_selec_arquivo.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky='nsew')
